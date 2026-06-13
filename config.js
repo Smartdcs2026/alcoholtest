45d886c7d49c32267ddcf40e137312bbf1395d61
SKDC_SMART <somchaibutphon@gmail.com>
2026-06-14 02:39:28 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,66 @@
+/************************************************************
+ * config.js
+ * การตั้งค่าฝั่ง Frontend
+ *
+ * ห้ามใส่ BACKEND_SECRET ในไฟล์นี้
+ ************************************************************/
+
+(function (window) {
+  'use strict';
+
+  const API_BASE =
+    'https://alcoholtest.somchaibutphon.workers.dev';
+
+  const CONFIG = {
+    APP_NAME:
+      'ระบบบันทึกการตรวจวัดปริมาณแอลกอฮอล์',
+
+    API_BASE:
+      API_BASE.replace(/\/+$/, ''),
+
+    TIMEZONE:
+      'Asia/Bangkok',
+
+    API_TIMEOUT_MS:
+      30000,
+
+    SAVE_TIMEOUT_MS:
+      120000,
+
+    ALERT_THRESHOLD:
+      1,
+
+    GAUGE_MAX:
+      50,
+
+    MAX_ROUNDS:
+      5,
+
+    IMAGE_MAX_WIDTH:
+      1280,
+
+    JPEG_QUALITY:
+      0.80,
+
+    MAX_IMAGE_BYTES:
+      2 * 1024 * 1024,
+
+    CAMERA: {
+      DEFAULT_FACING_MODE:
+        'environment',
+
+      IDEAL_WIDTH:
+        1920,
+
+      IDEAL_HEIGHT:
+        1080
+    },
+
+    DEBUG:
+      false
+  };
+
+  window.APP_CONFIG =
+    Object.freeze(CONFIG);
+
+})(window);
